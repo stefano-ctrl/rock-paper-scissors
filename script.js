a1c98476ff9547b3f6695b0f8bd84cf2a6fecded
Stefano <stefanopinna92@hotmail.it>
2022-07-22 14:56:25 +0000

    Added alert

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function game() {
     let scorePlayer = 0;
     let scoreComputer = 0;
     //let score = `${scorePlayer} to ${scoreComputer}`;
-    let finalScore = `${scorePlayer} to ${scoreComputer}`;
+    
     for (let i = 0; i < 5; i++){
 
         
@@ -76,25 +76,33 @@ function game() {
          
         if (result == 'You win motherfucker!' || result == 'Winner Winner Chicken Dinner' || result == 'You won Mate!' ) {
            
-               console.log(`${result}! -- Score for Player is ${scorePlayer += 1}`);
+               console.log(alert(`${result}! -- Score for Player is ${scorePlayer += 1}`));
              // console.log(result + ' ' + score);
+             console.log(scorePlayer + ' to ' + scoreComputer);
            } else if (result == 'Next time you\'ll have more luck' || result == 'Unlucky' || result == 'You should better quit') {
             
-             console.log(`${result}! -- Score for Computer is ${scoreComputer += 1}`);
+             console.log(alert(`${result}! -- Score for Computer is ${scoreComputer += 1}`));
              //console.log(result + ' ' +  score);
-        } else  {
-            console.log('Draw')
+             console.log(scorePlayer + ' to ' + scoreComputer);
+            } else  {
+            console.log(alert('Draw'))
            // console.log(score);
         };
-        console.log(finalScore);
+       
+     
+        };
+        if (scorePlayer > scoreComputer) {
+            console.log(alert(`The final score is: ${scorePlayer} to ${scoreComputer}. Player you are outrageous!`));
+        } else if (scorePlayer < scoreComputer) {
+            console.log(alert(`The final score is: ${scorePlayer} to ${scoreComputer}. Embarassing. Computer destroyed you!`));
+        } else {
+            console.log(alert(`The final score is: ${scorePlayer} to ${scoreComputer}. It's just another Draw. Try again`));
 };
-if (scorePlayer > scoreComputer) {
-    console.log(`The final score is: ${finalScore}. Player you are outrageous!`);
-} else if (scorePlayer < scoreComputer) {
-    console.log(`The final score is: ${finalScore}. Embarassing. Computer destroyed you!`);
-} else {
-    console.log(`The final score is: ${finalScore}. It's just another Draw. Try again`);
-};
+
 };
 
 console.log(game());
+
+
+// add an alert to determine who won round live
+// make sure that game is determined at who first wins 5 rounds
